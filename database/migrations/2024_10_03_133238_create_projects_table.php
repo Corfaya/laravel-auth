64e88810a4197923eff457a11f79915aeb5ce965
Corfaya <lauracorrado0696@gmail.com>
2024-10-03 16:04:05 +0200

    [UPD] migration: added columns

--- database/migrations/2024_10_03_133238_create_projects_table.php
+++ database/migrations/2024_10_03_133238_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->date('date_of_upload');
+            $table->text('stack');
+            $table->string('preview')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
