bfc9c1cc63fbec32fe6478a1481efeb14adf9395
Corfaya <lauracorrado0696@gmail.com>
2024-10-03 16:57:52 +0200

    [UPD] route for projects pages added + layout style added

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,5 @@
+<ul class="navbar-nav ms-4 mt-4">
+    <li class="nav-item">
+        <a class="nav-link text-white fs-5" href="{{route('admin.projects.index') }}">{{ __('Projects') }}</a>
+    </li>
+</ul>
