473644de6c5f4c6cd96b73499202c5fbb966cd18
Corfaya <lauracorrado0696@gmail.com>
2024-10-03 16:16:23 +0200

    [UPD] ProjectSeeder filled

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project as Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +14,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($k=0; $k<10; $k++) {
+            $new_project = new Project();
+            $new_project->name = $faker->word();
+            $new_project->date_of_upload = $faker->date();
+            $new_project->stack = $faker->randomElement(['HTML', 'CSS', 'JS', 'EJS', 'VUE', 'VITE', 'PHP']);
+            $new_project->preview = $faker->imageUrl(300, 400);
+            $new_project->description = $faker->sentence();
+
+            $new_project->save();
+        }
     }
 }
